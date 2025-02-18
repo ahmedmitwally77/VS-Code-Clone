a3abdc74c5c571efcc068866be034062073ca31a
ahmedmitwally77 <anaahmedmito5@gmail.com>
2025-02-18 15:24:18 +0200

    Added missing icons

--- src/Components/FolderIcon/FolderIcon.tsx
+++ src/Components/FolderIcon/FolderIcon.tsx
@@ -9,24 +9,24 @@ interface IProps {
 export default function FolderIcon({ name ,isOpen }: IProps) {
 
   if(name == "Node_Module" && !isOpen)
-    return <img src='../../../icons/folder-node-open.svg'/>
+    return <img src='/icons/folder-node-open.svg'/>
   if(name == "Node_Module" && isOpen)
-    return <img src='../../../icons/folder-node.svg'/>
+    return <img src='/icons/folder-node.svg'/>
 
   if(name == "public" && !isOpen)
-    return <img src='../../../icons/folder-public-open.svg'/>
+    return <img src='/icons/folder-public-open.svg'/>
   if(name == "public" && isOpen)
-    return <img src='../../../icons/folder-public.svg'/>
+    return <img src='/icons/folder-public.svg'/>
 
   if(name == "src" && !isOpen)
-    return <img src='../../../icons/folder-src-open.svg'/>
+    return <img src='/icons/folder-src-open.svg'/>
   if(name == "src" && isOpen)
-    return <img src='../../../icons/folder-src.svg'/>
+    return <img src='/icons/folder-src.svg'/>
 
   if(name == "Components" && !isOpen)
-    return <img src='../../../icons/folder-components-open.svg'/>
+    return <img src='/icons/folder-components-open.svg'/>
   if(name == "Components" && isOpen)
-    return <img src='../../../icons/folder-components.svg'/>
+    return <img src='/icons/folder-components.svg'/>
 
   if(isOpen)
     return <FaFolderClosed className="text-yellow-500"/>
