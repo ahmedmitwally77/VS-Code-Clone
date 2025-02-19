22d330fadce7d57c6d6cebc372654b3feca1d366
ahmedmitwally77 <anaahmedmito5@gmail.com>
2025-02-19 15:16:53 +0200

    update Responsive

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const { opendTabs } = useAppSelector((state) => state.FileTreeSlice);
   return (
     <>
-      <div className="app flex flex-wrap text-white">
+      <div className="app flex flex-wrap text-white min-h-screen ">
         <div className="aside-navs w-full lg:w-1/6 md:w-1/4 sm:w-1/3 sm:h-screen  p-4 bg-gray-800">
           <FileComponent fileTree={fileTree} />
         </div>
